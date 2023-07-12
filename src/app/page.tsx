5be6c467d33ba496283b62f071d5ee9c8ac03329
alfredo <alfredofontinele08@gmail.com>
2023-07-12 20:53:36 -0300

    feat: front concepts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import BackgroundImg from "@/assets/background.jpg"
+import FormComponent from "@/components/Form"
+
+import { CSSObject, Flex, MediaQuery } from "@mantine/core"
+import { useMediaQuery } from "@mantine/hooks"
+import Image from "next/image"
 
 export default function Home() {
+  const matches = useMediaQuery("(min-width: 94rem)")
+
+  const highlight: CSSObject = {
+    display: "none",
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main>
+      <Flex mih={"100vh"} justify="space-between" align="center">
+        {matches ? (
+          <Flex w={"40%"}>
+            <FormComponent />
+          </Flex>
+        ) : (
+          <Flex w={"100%"} justify={"center"}>
+            <FormComponent />
+          </Flex>
+        )}
+        <MediaQuery smallerThan={"xl"} styles={highlight}>
+          <Flex w={"60%"}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              style={{
+                height: "100vh",
+                width: "100%",
+              }}
+              src={BackgroundImg}
+              alt={"background image"}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          </Flex>
+        </MediaQuery>
+      </Flex>
     </main>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,167 @@
+import PlyrmeImg from "@/assets/Plyrme.png"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { Anchor, Button, Checkbox, Grid, Text, TextInput } from "@mantine/core"
+import Image from "next/image"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+
+interface DataProps {
+  name: string
+  email: string
+  phone: string
+  password: string
+  confirm_password: string
+  club_name: string
+}
+
+export default function FormComponent() {
+  const [checkboxStatus, setCheckBoxStatus] = useState(false)
+  const [phone, setPhone] = useState("")
+
+  const FormSchema = yup.object({
+    name: yup.string().required("Nome Obrigatório"),
+    email: yup
+      .string()
+      .required("Email obrigatório")
+      .email("Email obrigatório"),
+    phone: yup.string().required("Celular obrigatório"),
+    password: yup
+      .string()
+      .required("Senha obrigatória")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/,
+        "Senha com no mínimo 8 caracteres. Necessário ter letras, números e ao menos um símbolo"
+      ),
+    confirm_password: yup
+      .string()
+      .required("Confirme sua Senha")
+      .oneOf([yup.ref("password")], "Senhas precisam ser iguais"),
+    club_name: yup.string().required("Nome do Clube Obrigatório"),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(FormSchema),
+  })
+
+  const onSubmitForm = (data: DataProps) => {
+    const newData = { ...data, club_name: data.club_name, checkboxStatus }
+    console.log(newData)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmitForm)}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        gap: 20,
+        padding: 40,
+        minHeight: "100vh",
+        maxWidth: "700px",
+      }}
+    >
+      <Image src={PlyrmeImg} alt={"logo image"} />
+      <Text
+        sx={{ fontFamily: "Roboto, sans-serif" }}
+        color="#666"
+        ta="center"
+        fz="xl"
+        fw={700}
+      >
+        Crie seu clube na plyr.me
+      </Text>
+      <Grid>
+        <Grid.Col span={12}>
+          <TextInput
+            label="Name"
+            placeholder="Digite seu nome"
+            error={errors.name && errors.name.message}
+            mt="sm"
+            {...register("name")}
+          />
+        </Grid.Col>
+        <Grid.Col span={7}>
+          <TextInput
+            label="Email"
+            placeholder="Digite seu email"
+            error={errors.email && errors.email.message}
+            mt="sm"
+            {...register("email")}
+          />
+        </Grid.Col>
+        <Grid.Col span={5}>
+          <TextInput
+            label="Celular"
+            placeholder="(00) 00000-0000"
+            mt="sm"
+            error={errors.phone && errors.phone.message}
+            {...register("phone")}
+          />
+        </Grid.Col>
+
+        <Grid.Col span={5}>
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            error={errors.password && errors.password.message}
+            mt="sm"
+            {...register("password")}
+          />
+        </Grid.Col>
+
+        <Grid.Col span={7}>
+          <TextInput
+            label="Confirmar Senha"
+            placeholder="Confirme sua senha"
+            error={errors.confirm_password && errors.confirm_password.message}
+            mt="sm"
+            {...register("confirm_password")}
+          />
+        </Grid.Col>
+
+        <Grid.Col span={10}>
+          <TextInput
+            label="Nome do seu clube"
+            placeholder="Digite o nome do seu clube"
+            error={errors.club_name && errors.club_name.message}
+            mt="sm"
+            {...register("club_name")}
+          />
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <Text mt={50}>.plyr.me</Text>
+        </Grid.Col>
+      </Grid>
+      <Checkbox
+        onClick={(e) => setCheckBoxStatus((value) => !value)}
+        required
+        label={
+          <Text>
+            Aceito os {}
+            <Anchor href="https://mantine.dev" target="_blank" color="orange">
+              Termos e Condições
+            </Anchor>
+            {} da Plyr.me e confirmo que não publicarei conteúdo adulto no meu
+            clube.
+          </Text>
+        }
+      />
+      <Button type="submit" color="orange">
+        CRIAR CONTA
+      </Button>
+      <Text>
+        Já tem um clube? {}
+        <Anchor href="https://mantine.dev" target="_blank" color="orange">
+          Faça o Login
+        </Anchor>
+      </Text>
+    </form>
+  )
+}
